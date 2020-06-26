8c35ec00f60575f1d7daca38cc7bd2440372c785
ZhangChaofan <2170850451@qq.com>
2020-06-26 15:32:33 +0800

    添加模板方法模式

---
+++ src/template/BlackBeanSoymilkMake.java
@@ -0,0 +1,15 @@
+package template;
+
+/**
+ * 黑豆豆浆的制作类
+ * 
+ * @author Van
+ */
+public class BlackBeanSoymilkMake extends SoymilkMakeTemplate {
+
+	@Override
+	public void addCondiment() {
+		System.out.println("加入黑豆");
+	}
+
+}
